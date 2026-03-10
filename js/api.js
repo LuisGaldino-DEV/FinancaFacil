ffd175e9bdd969b50a8ee63dbff1964757725373
Luis Galdino <luisgaldino.dev@gmail.com>
2026-03-10 12:17:27 -0300

    Subida aruivos js

--- js/api.js
+++ js/api.js
@@ -1,21 +1,20 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbybPAaC_KmS38XVO1VyjCUwkh-uN7kh6O3MT49h8vtfCicp0AYegQJXBMnDTAnKtrJoyg/exec";
+const API_URL = "http://localhost:3000/api"; // proxy local
 
-const API = {
-  get: async function(action, params = {}) {
-    const email = localStorage.getItem("email");
-    params.email = email;
-    const query = new URLSearchParams({ action, ...params }).toString();
-    const res = await fetch(`${API_URL}?${query}`);
-    return res.json();
-  },
-  post: async function(action, data = {}) {
-    data.email = localStorage.getItem("email");
-    data.action = action;
-    const res = await fetch(API_URL, {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: { "Content-Type": "application/json" }
-    });
-    return res.json();
-  }
-};
+async function getData(action, params = {}) {
+  const email = localStorage.getItem("email");
+  params.email = email;
+  const query = new URLSearchParams({ action, ...params }).toString();
+  const res = await fetch(`${API_URL}?${query}`);
+  return res.json();
+}
+
+async function postData(action, payload) {
+  payload.email = localStorage.getItem("email");
+  payload.action = action;
+  const res = await fetch(API_URL, {
+    method: "POST",
+    body: JSON.stringify(payload),
+    headers: { "Content-Type": "application/json" }
+  });
+  return res.json();
+}
